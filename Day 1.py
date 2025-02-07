5a71e163c1b04258f6cefdc31a2eafa31abad4b8
Aaron <aaronchinosrs@gmail.com>
2025-02-07 21:41:11 +0800

    feat: sort left and right lists of location IDs
    This feature initialises all locations into their respective left and
    right list, then sorts it.

--- Day 1.py
+++ Day 1.py
@@ -1,3 +1,18 @@
 class location:
     def __init__(self, ID):
         self.ID = int(ID)
+
+def init_locations(input_file):
+    locations_1 = []
+    locations_2 = []
+    with open("r", input_file) as fileref:
+        for line in fileref:
+            location_1, location_2 = line.strip().split()
+            locations_1.append(location(location_1))
+            locations_2.append(location(location_2))
+
+    locations_1.sort(key = lambda location: location.ID)
+    locations_2.sort(key = lambda location: location.ID)
+
+    return locations_1, locations_2
+
