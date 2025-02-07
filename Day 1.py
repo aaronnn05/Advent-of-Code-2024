487dc0696aae0ff694afae322a72750574a6d149
Aaron <aaronchinosrs@gmail.com>
2025-02-08 00:46:22 +0800

    refactor: optimize and shorten overall code

--- Day 1.py
+++ Day 1.py
@@ -1,39 +1,16 @@
-class location:
-    def __init__(self, ID):
-        self.ID = int(ID)
-
-def init_locations(input_file):
-    locations_1 = []
-    locations_2 = []
-    with open(input_file, "r") as fileref:
-        for line in fileref:
-            location_1, location_2 = line.strip().split()
-            locations_1.append(location(location_1))
-            locations_2.append(location(location_2))
-
-    locations_1.sort(key = lambda location: location.ID)
-    locations_2.sort(key = lambda location: location.ID)
-
-    return locations_1, locations_2
+def load_locations(file_path):
+    with open(file_path, "r") as fileref:
+        locations_1, locations_2 = zip(*[map(int, line.strip().split()) for line in fileref])
+    return sorted(locations_1), sorted(locations_2)
 
 def calculate_total_distance(locations_1, locations_2):
-    total_distance = 0
-    for num in range(len(locations_1)):
-        pair_distance = abs(locations_1[num].ID - locations_2[num].ID)
-        total_distance += pair_distance
-    
-    return total_distance
+    return sum(abs(locations_1[i] - locations_2[i]) for i in range(len(locations_1)))
 
 def calculate_similarity_score(locations_1, locations_2):
-    similarity_score = 0
-    for location_1 in locations_1:
-        similarity_score += location_1.ID * \
-            sum(1 for location_2 in locations_2 if location_2.ID == location_1.ID)
-
-    return similarity_score
+    return sum(loc1 * sum(1 for loc2 in locations_2 if loc2 == loc1) for loc1 in locations_1)
 
 def main():
-    locations_1, locations_2 = init_locations("Day 1 Input.txt")
+    locations_1, locations_2 = load_locations("Day 1 Input.txt")
     total_distance = calculate_total_distance(locations_1, locations_2)
     similarity_score = calculate_similarity_score(locations_1, locations_2)
     print(f"Total distance: {total_distance}")
