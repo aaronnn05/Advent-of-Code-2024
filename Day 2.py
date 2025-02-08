4bebb560f2da4bcd3b5f05410cca2e6a136d612e
Aaron <aaronchinosrs@gmail.com>
2025-02-08 16:55:54 +0800

    feat: check if the reports are safe or unsafe
    Create a recursive function to check the reports based on the two rules.
    Said function will return True if the respective report is safe; vice versa.

--- Day 2.py
+++ Day 2.py
@@ -4,3 +4,15 @@ def load_reports(file_path):
 
     return reports
 
+def check_rules_recursive(report):
+    increasing, decreasing = True, True
+    if len(report) == 1:
+        return True
+    elif (report[0] < report[1]) and 1 <= (report[1] - report[0]) <= 3 and increasing:
+        decreasing = False
+        return check_rules_recursive(report[1:])
+    elif (report[0] > report[1]) and 1 <= (report[0] - report[1]) <= 3 and decreasing:
+        increasing = False
+        return check_rules_recursive(report[1:])
+    else:
+        return False
