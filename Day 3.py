1edee07ca13cce0f0e0428d4859f337e29c70a4a
Aaron Chin <135807260+aaronnn05@users.noreply.github.com>
2025-02-18 03:47:57 +0800

    feat: ignores all mul() after don't() (#5)

--- Day 3.py
+++ Day 3.py
@@ -2,10 +2,28 @@ import re
 
 def add_mul(file_path):
     with open(file_path, "r") as file:
-        return sum(int(a)*int(b) for a, b in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", file.read()))
+        complete_memory = re.sub(r"don't\(\).*?(do\(\)|$)", "", file.read(), flags=re.DOTALL)
+                                                        #".*? doesn't match \n without re.DOTALL"
+        return sum(int(a)*int(b) for a, b in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", complete_memory))
                                                         #Use r"" when using \ in regex to prevent conflicts with python's \
 def main():
-    print(f"Results of multiplications: {add_mul("Day 3 Input.txt")}")
+    print(f"Results of multiplications: {add_mul('Day 3 Input.txt')}")
 
 if __name__ == "__main__":
     main()
+
+# total1 = total2 = 0
+# enabled = True
+# data = open(...).read()
+
+# for a, b, do, dont in findall(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))", data):
+#     if do or dont:
+#         enabled = bool(do)
+#     else:
+#         x = int(a) * int(b)
+#         total1 += x
+#         total2 += x * enabled
+
+# print(total1, total2)
+
+
