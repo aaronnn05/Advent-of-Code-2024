9bc01c83080f0d44b1cec8d7a56b67f57edc7d2b
Aaron <aaronchinosrs@gmail.com>
2025-02-18 21:28:15 +0800

    feat: parse the puzzle and find total number of X-MAS

--- Day 4.py
+++ Day 4.py
@@ -2,18 +2,12 @@ def load_puzzle(file_path):
     with open(file_path, "r") as file:
         return [line.strip() for line in file]
     
-def parse_puzzle(table, total = 0):
-    variants = ["XMAS", "SAMX"]
-    idx_lsts = [[(0, 0), (0, 1), (0, 2), (0, 3)], #Vertical
-               [(0, 0), (1, 0), (2, 0), (3, 0)],  #Horizontal
-               [(0, 0), (1, 1), (2, 2), (3, 3)],  #Top Right
-               [(0, 0), (1, -1), (2, -2), (3, -3)]] #Bottom Right
-
-    for row_idx in range(len(table)):
-        for col_idx in range(len(table[0])):
+def parse_puzzle(puzzle, variants, idx_lsts, total = 0):
+    for y in range(len(puzzle)):
+        for x in range(len(puzzle[0])):
             for idx_lst in idx_lsts:
                 try:
-                    if "".join(table[row_idx + dy][col_idx + dx] for dy, dx in idx_lst) in variants:
+                    if "".join(puzzle[y + dy][x + dx] for dx, dy in idx_lst) in variants:
                         total += 1
                 except IndexError:
                     pass
@@ -21,8 +15,18 @@ def parse_puzzle(table, total = 0):
     return total
 
 def main():
+    variants1 = ["XMAS", "SAMX"]
+    variants2 = ["MASMS", "SAMMS", "MASSM", "SAMSM"]
+    idx_lsts1 = [[(0, 0), (0, 1), (0, 2), (0, 3)], #Vertical
+               [(0, 0), (1, 0), (2, 0), (3, 0)],  #Horizontal
+               [(0, 0), (1, 1), (2, 2), (3, 3)],  #Diagonal
+               [(0, 3), (1, 2), (2, 1), (3, 0)]]  #Other Diagonal
+    idx_lsts2 = [[(0, 0), (1, 1), (2, 2), (0, 2), (2, 0)]]
+    #Cannot use negative indices cuz python will take it literally instead of going out of bounds
+    
     puzzle = load_puzzle("Day 4 Input.txt")
-    print(f"Total times that XMAS appear: {parse_puzzle(puzzle)}")
+    print(f"Total times that XMAS appear: {parse_puzzle(puzzle, variants1, idx_lsts1)}")
+    print(f"Total times that X-MAS appear: {parse_puzzle(puzzle, variants2, idx_lsts2)}")
 
 if __name__ == "__main__":
     main()
