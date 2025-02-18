68cdec3165ca33ff64df80748b6b7d04d3fdc644
Aaron <aaronchinosrs@gmail.com>
2025-02-18 20:17:36 +0800

    feat: parse the puzzle and get total number of XMAS

--- Day 4.py
+++ Day 4.py
@@ -1,8 +1,28 @@
 def load_puzzle(file_path):
     with open(file_path, "r") as file:
-        return [line.strip() for line in file.readlines()]
+        return [line.strip() for line in file]
     
 def parse_puzzle(table, total = 0):
+    variants = ["XMAS", "SAMX"]
+    idx_lsts = [[(0, 0), (0, 1), (0, 2), (0, 3)], #Vertical
+               [(0, 0), (1, 0), (2, 0), (3, 0)],  #Horizontal
+               [(0, 0), (1, 1), (2, 2), (3, 3)],  #Top Right
+               [(0, 0), (1, -1), (2, -2), (3, -3)]] #Bottom Right
+
     for row_idx in range(len(table)):
-        total += sum(search_everywhere(table, row_idx, col_idx) for col_idx in range(len(table[row_idx])) if table[row_idx][col_idx] == "X")
+        for col_idx in range(len(table[0])):
+            for idx_lst in idx_lsts:
+                try:
+                    if "".join(table[row_idx + dy][col_idx + dx] for dy, dx in idx_lst) in variants:
+                        total += 1
+                except IndexError:
+                    pass
+                
     return total
+
+def main():
+    puzzle = load_puzzle("Day 4 Input.txt")
+    print(f"Total times that XMAS appear: {parse_puzzle(puzzle)}")
+
+if __name__ == "__main__":
+    main()
