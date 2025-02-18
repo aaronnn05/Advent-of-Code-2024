65be24205f48a977a743201acea1660feb46c1b1
Aaron <aaronchinosrs@gmail.com>
2025-02-18 17:39:35 +0800

    feat: iterate thru the puzzle to find 'X's

--- Day 4.py
+++ Day 4.py
@@ -1,3 +1,8 @@
 def load_puzzle(file_path):
     with open(file_path, "r") as file:
         return [line.strip() for line in file.readlines()]
+    
+def parse_puzzle(table, total = 0):
+    for row_idx in range(len(table)):
+        total += sum(search_everywhere(table, row_idx, col_idx) for col_idx in range(len(table[row_idx])) if table[row_idx][col_idx] == "X")
+    return total
