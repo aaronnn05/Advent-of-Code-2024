db6ec48a49195bd4bc1899c237d77973886d1210
Aaron <aaronchinosrs@gmail.com>
2025-02-19 19:58:21 +0800

    feat: generate total x and y, and directions

--- Day 4a.py
+++ Day 4a.py
@@ -1,3 +1,10 @@
 def load_puzzle(file_path):
     with open(file_path, "r") as file:
         return [line.strip() for line in file]
+    
+def generate_directions(puzzle):
+    total_x = len(puzzle[0])
+    total_y = len(puzzle)
+    xy_directions = [(dx, dy) for dx, dy in range(-1, 2) if not (dx == 0 and dy == 0)]
+
+    return total_x, total_y, xy_directions
