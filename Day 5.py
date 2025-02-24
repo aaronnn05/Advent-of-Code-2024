7fbb2f08aa090c9a2ac130f2aa9d6044b24c0ba0
Aaron <aaronchinosrs@gmail.com>
2025-02-24 21:19:48 +0800

    feat: parse through all updates to check

--- Day 5.py
+++ Day 5.py
@@ -1,6 +1,6 @@
 def load_rules(file_path):
     with open(file_path, "r") as file:
-        return [line.strip().split("|") for line in file]
+        return {tuple(line.strip().split("|")) for line in file}
     
 def load_pages(file_path):
     with open(file_path, "r") as file:
@@ -9,10 +9,18 @@ def load_pages(file_path):
 def check_rules(rules, update):
     for i in range(len(update)):
         for j in range(i, len(update)):
-            if [update[j], update[i]] in rules:
+            if (update[j], update[i]) in rules:
                 return False
     return True
 
+def parse_updates(rules, updates):
+    return sum(int(update[len(update)//2]) for update in updates if check_rules(rules, update))
+
 def main():
-    ordering_rules = load_rules("Day 5 Input 2.txt")
-    pages_update = load_pages("Day 5 Input 1.txt")
+    ordering_rules = load_rules("Day 5 Input 1.txt")
+    pages_updates = load_pages("Day 5 Input 2.txt")
+    total = parse_updates(ordering_rules, pages_updates)
+    print(f"Total middle page number: {total}")
+
+if __name__ == "__main__":
+    main()
