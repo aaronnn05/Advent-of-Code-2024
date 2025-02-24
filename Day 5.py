125acc75f1a8611ea34111a3213368b3049e7c17
Aaron <aaronchinosrs@gmail.com>
2025-02-24 20:38:13 +0800

    feat: load pages to update into a list

--- Day 5.py
+++ Day 5.py
@@ -2,3 +2,10 @@ def load_rules(file_path):
     with open(file_path, "r") as file:
         return [line.strip().split("|") for line in file]
     
+def load_pages(file_path):
+    with open(file_path, "r") as file:
+        return [line.strip().split(",") for line in file]
+
+def main():
+    ordering_rules = load_rules("Day 5 Input 2.txt")
+    pages_update = load_pages("Day 5 Input 1.txt")
