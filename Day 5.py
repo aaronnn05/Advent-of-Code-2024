dc8795f472331c7c241f7b88526c89593d41f9c7
Aaron <aaronchinosrs@gmail.com>
2025-02-24 21:00:50 +0800

    feat: check if an update abide the rules

--- Day 5.py
+++ Day 5.py
@@ -5,6 +5,13 @@ def load_rules(file_path):
 def load_pages(file_path):
     with open(file_path, "r") as file:
         return [line.strip().split(",") for line in file]
+    
+def check_rules(rules, update):
+    for i in range(len(update)):
+        for j in range(i, len(update)):
+            if [update[j], update[i]] in rules:
+                return False
+    return True
 
 def main():
     ordering_rules = load_rules("Day 5 Input 2.txt")
