29ce2bd6d5c0596a1d680df87611b48a24b6c744
Aaron <aaronchinosrs@gmail.com>
2025-02-25 21:40:27 +0800

    feat: re-organise incorrect updates and total up their middle page

--- Day 5b.py
+++ Day 5b.py
@@ -6,15 +6,26 @@ def load_pages(file_path):
     with open(file_path, "r") as file:
         return [line.strip().split(",") for line in file]
     
-def check_rules(rules, update):
+def abide_rules(rules, update):
     return (
         len(update) == 1
         or not any((update[i], update[0]) in rules for i in range(len(update)))
-        and check_rules(rules, update[1:])
+        and abide_rules(rules, update[1:])
     )
 
 def parse_updates(rules, updates):
-    return sum(int(update[len(update)//2]) for update in updates if check_rules(rules, update))
+    total = 0
+    for update in updates:
+        if abide_rules(rules, update):
+            continue
+        else:
+            while not abide_rules(rules, update):
+                for i in range(len(update)):
+                    for j in range(i, len(update)):
+                        if (update[j], update[i]) in rules:
+                            update[i], update[j] = update[j], update[i]     #Bubble sort
+            total += int(update[len(update)//2])
+    return total
 
 def main():
     ordering_rules = load_rules("Day 5 Input 1.txt")
